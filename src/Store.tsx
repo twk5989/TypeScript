85d735ea8c6ab31ff92174b08e8c3500b5a687b6
twk5989 <a37225989@gmail.com>
2025-06-25 22:44:19 +0900

    연습

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Store:React.FC = () => {
+const Store:React.FC  = () => {
     return(
         <div>Store</div>
     )
 }
-
+//여기서도 지정을 해줘야함 
 export default Store
